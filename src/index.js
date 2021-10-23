ab3a31ebbce6f89e465e3d5fa1c89b55441ef6ec
Fahim Faisal <fahimfaisal.soikot@gmail.com>
2021-10-23 22:33:05 +0600

    feat: version 1.0

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import { gsap } from "gsap";
+import { TextPlugin } from "gsap/TextPlugin";
+import Animations from './scripts/Animations';
 import DigitalRoot from './scripts/digitalRoot';
 import Utility from './scripts/utility';
 import './styles/style.scss';
@@ -9,3 +12,10 @@ const digitalRoot = new DigitalRoot(inputNode);
 
 // run the event 👍
 digitalRoot.runInputEvent();
+
+gsap.registerPlugin(TextPlugin);
+
+const animations = new Animations();
+animations.runIntroAnimation()
+
+// TODO: add helper text animation
