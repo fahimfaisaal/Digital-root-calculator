39986032f3e67b64abfafad8b166f870e1b211b8
Fahim Faisal <fahimfaisal.soikot@gmail.com>
2021-10-24 13:23:00 +0600

    feat: re-modify the animations

--- src/index.js
+++ src/index.js
@@ -2,20 +2,30 @@ import { gsap } from "gsap";
 import { TextPlugin } from "gsap/TextPlugin";
 import Animations from './scripts/Animations';
 import DigitalRoot from './scripts/digitalRoot';
+import headingSVG from './scripts/headingSVG';
 import Utility from './scripts/utility';
 import './styles/style.scss';
 
 const { $ } = Utility;
+
+gsap.registerPlugin(TextPlugin);
+
+const h2 = $('h2');
+h2.innerText = "Results"
+// replace the h2 tag letter by letter
+h2.innerHTML = h2.innerText.replace(/\S/g, '<span class="result-split-heading">$&</span>');
+
+// render svg into svg wrapper
+const headingWrapper = $('.svg-wrapper');
+headingWrapper.innerHTML = headingSVG;
+
+const animations = new Animations();
+animations.runIntroAnimation()
 
 const inputNode = $('#inputNumber');
 const digitalRoot = new DigitalRoot(inputNode);
 
 // run the event 👍
 digitalRoot.runInputEvent();
-
-gsap.registerPlugin(TextPlugin);
-
-const animations = new Animations();
-animations.runIntroAnimation()
 
 // TODO: add helper text animation
